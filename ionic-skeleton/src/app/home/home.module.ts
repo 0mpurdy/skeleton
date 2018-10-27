ffc443f703190ab946356af0b85d7832e7512288
Michael Purdy <0mpurdy@gmail.com>
2018-10-27 01:07:34 +0100

    Add ionic 4 tabs with disabling input

---
+++ ionic-skeleton/src/app/home/home.module.ts
@@ -0,0 +1,17 @@
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HomePage } from './home.page';
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule.forChild([{ path: '', component: HomePage }])
+  ],
+  declarations: [HomePage]
+})
+export class HomePageModule {}
---
+++ ionic-skeleton/src/app/home/home.page.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss']
+})
+export class HomePage implements OnInit {
+
+  private homeForm: FormGroup;
+  private disableEmail: Subject<boolean>;
+
+  constructor(private fb: FormBuilder) { }
+
+  public ngOnInit() {
+    this.homeForm = this.fb.group({
+      email: '',
+      notEmail: { value: 'something', disabled: true},
+    });
+
+    this.disableEmail = new Subject();
+    this.disableEmail
+      .pipe(distinctUntilChanged())
+      .subscribe(this.setNotEmailDisabled);
+
+    this.homeForm.valueChanges
+      .pipe(debounceTime(100))
+      .subscribe(this.handleFormChanges);
+  }
+
+  private setNotEmailDisabled = (disable: boolean) => {
+    if (disable) {
+      this.homeForm.get('notEmail').disable();
+    } else {
+      this.homeForm.get('notEmail').enable();
+    }
+  }
+
+  private handleFormChanges = (changes: any) => {
+    console.log('changes', changes);
+    this.disableEmail.next(!!this.homeForm.get('email').value);
+  }
+}

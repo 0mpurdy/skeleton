08f60f3608cdb82bf7e8e43d3a29159e729c87fa
Michael Purdy <0mpurdy@gmail.com>
2018-10-27 01:38:25 +0100

    Experiment with fakeAsync tests

--- ionic-skeleton/src/app/home/home.page.spec.ts
+++ ionic-skeleton/src/app/home/home.page.spec.ts
@@ -1,7 +1,9 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 
 import { HomePage } from './home.page';
+import { IonicModule } from '@ionic/angular';
 
 describe('HomePage', () => {
   let component: HomePage;
@@ -9,6 +11,7 @@ describe('HomePage', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [IonicModule.forRoot(), ReactiveFormsModule],
       declarations: [HomePage],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
@@ -22,5 +25,44 @@ describe('HomePage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have disabled notEmail when there are characters in email', () => {
+    // arrange
+    // act
+    component.homeForm.get('email').setValue('something');
+    // assert
+    expect(component.homeForm.get('notEmail').disabled).toBe(true);
+  });
+
+  it('should have enabled notEmail when there are no characters in email', fakeAsync(() => {
+    // arrange
+    fixture.detectChanges();
+    // act
+    component.homeForm.get('email').setValue('');
+    console.log('ticking');
+    tick(200);
+    fixture.detectChanges();
+    // assert
+    expect(component.homeForm.get('notEmail').disabled).toBe(false);
+  }));
+
+  it('should have the same date', fakeAsync(() => {
+    // arrange
+    const startTime = new Date();
+    // act
+    const endTime = new Date();
+    // assert
+    expect(startTime).toEqual(endTime);
+  }));
+
+  it('should have different dates', fakeAsync(() => {
+    // arrange
+    const startTime = new Date();
+    tick(100);
+    // act
+    const endTime = new Date();
+    // assert
+    expect(startTime).not.toEqual(endTime);
   });
 });
--- ionic-skeleton/src/app/home/home.page.ts
+++ ionic-skeleton/src/app/home/home.page.ts
@@ -10,7 +10,7 @@ import { Subject } from 'rxjs';
 })
 export class HomePage implements OnInit {
 
-  private homeForm: FormGroup;
+  public homeForm: FormGroup;
   private disableEmail: Subject<boolean>;
 
   constructor(private fb: FormBuilder) { }
